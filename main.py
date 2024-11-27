a18e86a0b9641a4faecd17b7ae099e578b5c719b
Fuwuffy <palazzini21@gmail.com>
2024-11-27 15:09:17 +0100

    Start ui abstraction to another file

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 #!/bin/python
 
 import curses
+import ui 
 from router import Router
 from collections import OrderedDict
 from networkmanager import read_network_file
 
 def run_distance_vector(routers: dict[str, Router], t_max: int):
     tables: dict[int, OrderedDict[str, OrderedDict[str, tuple[str, int]]]] = dict()
-    for t in range(0, len(routers)):
+    # Initialize T=0
+    tables[0] = OrderedDict()
+    for id, router in routers.items():
+        tables[0][id] = router.get_frozen_table()
+    # Start routing table exchange
+    for t in range(1, len(routers)):
         # Create the new container for the tables at t
         tables[t] = OrderedDict()
         # Run the algorithm
@@ -27,44 +33,31 @@ if __name__ == '__main__':
     routers: dict[str, Router] = read_network_file("test_network.txt")
     # Run the algorithm and save all the steps
     tables: dict[int, OrderedDict[frozenset[str], tuple[str, int]]] = run_distance_vector(routers=routers, t_max=len(routers))
-    
-    # Function used to draw the UI
-    def main(stdscr: curses.window) -> any:
-        # Set some flags for the UI
-        curses.curs_set(0)
-        stdscr.nodelay(0)
-        stdscr.timeout(100)
-        cursor_position: tuple[int, int] = (0, 0)
-        # Draw the UI
-        while True:
-            # Get user input
-            key = stdscr.getch()
-            # Exit on Q
-            if key == ord('q'):
-                break
-            elif key == curses.KEY_RIGHT or key == ord('d'):
-                # Move RIGHT
-                cursor_position = (cursor_position[0] + 1 if cursor_position[0] + 1 < len(routers) else len(routers) - 1, cursor_position[1])
-            elif key == curses.KEY_LEFT or key == ord('a'):
-                # Move LEFT 
-                cursor_position = (cursor_position[0] - 1 if cursor_position[0] - 1 >= 0 else 0, cursor_position[1])
-            elif key == curses.KEY_UP or key == ord('w'):
-                # Move UP 
-                cursor_position = (cursor_position[0], cursor_position[1] - 1 if cursor_position[1] - 1 >= 0 else 0)
-            elif key == curses.KEY_DOWN or key == ord('s'):
-                # Move DOWN 
-                cursor_position = (cursor_position[0], cursor_position[1] + 1) # TODO: add other bound
-            # Clear the screen
-            stdscr.clear()
-            # Draw the UI
-            stdscr.addstr(0, 0, f"Current page: t = {cursor_position[0]}")
-            # Refresh the screepair source, dest,n
-            stdscr.refresh()
-
-    # TODO: uncomment once UI is done
-    # Start the UI
-    # curses.wrapper(main)
-    # tables: dict[int, OrderedDict[str, OrderedDict[str, tuple[str, int]]]] = dict()
+    # Initialize the UI
+    stdscr = ui.init()
+    # Create the window to make sure it is the right dimensions
+    orig_rows, orig_cols = stdscr.getmaxyx()
+    window: curses.window = curses.newwin(orig_rows - 2, orig_cols - 2, 1, 1)
+    cursor_position: tuple[int, int] = (0, 0)
+    total_pages: int = len(routers) - 1
+    # Draw the UI
+    quit_ui: bool = False
+    while not quit_ui:
+        # Resize window
+        rows, cols = stdscr.getmaxyx()
+        if orig_cols != cols or orig_rows != rows:
+            orig_rows, orig_cols = rows, cols
+            window.resize(orig_rows - 2, orig_cols - 2)
+        # Get user input
+        cursor_position = ui.handle_user_input(stdscr, cursor_position, total_pages)
+        if cursor_position[0] == -1:
+            quit_ui = True
+        # Clear the screen
+        stdscr.clear()
+        # Refresh the screen
+        stdscr.refresh()
+    curses.endwin()
+    """
     for time, curr_tables in tables.items():
         print(f"Tables at time: {time}")
         for router_id, routing_table in curr_tables.items():
@@ -75,3 +68,4 @@ if __name__ == '__main__':
                 else:
                     next_hop, cost = routing_table.get(dest) if dest in routing_table else ("NONE", "INF")
                     print(f"Dest: {dest}, N.Hop: {next_hop}, Cost: {cost if cost else 'INF'}")
+    """
---
+++ ui.py
@@ -0,0 +1,41 @@
+import curses
+from curses.textpad import rectangle as guirect
+
+def init() -> curses.window:
+    stdscr = curses.initscr()
+    curses.curs_set(0)
+    curses.cbreak()
+    curses.noecho()
+    # stdscr.nodelay()
+    # stdscr.timeout(100)
+    # curses.start_color()  # Initialize color support
+    # curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)  # Blue on black for header
+    # curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)  # White on black for text
+    # curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)  # Yellow for highlighting
+    return stdscr
+
+def handle_user_input(stdscr: curses.window, cursor_position: tuple[int, int], total_pages: int) -> tuple[int, int]:
+    # Read key input from user (TODO: arrows do not work, fix)
+    key: int = stdscr.getch()
+    # Exit if q or esc pressed
+    if key == ord('q') or key == 27:
+        return (-1, -1)
+    elif key == ord('d'):
+        # Next page
+        return (min(cursor_position[0] + 1, total_pages), cursor_position[1])
+    elif key == ord('a'):
+        # Previous page
+        return (max(cursor_position[0] - 1, 0), cursor_position[1])
+    elif key == ord('w'):
+        # Scroll up
+        return (cursor_position[0], max(cursor_position[1] - 1, 0))
+    elif key == ord('s'):
+        # Scroll down
+        return (cursor_position[0], cursor_position[1] + 1) # TODO: add minimum here
+    elif key == ord('h'):
+        # Show help menu
+        stdscr.clear()
+        stdscr.addstr(1, 1, "Help: Use 'a/arrow left' for previous page, 'd/arrow right' for next page, 'q/esc' to quit.")
+        stdscr.refresh()
+        stdscr.getch()
+    return cursor_position
